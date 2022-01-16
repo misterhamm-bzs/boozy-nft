cc446c78eb13188eba9aacf8b9a85eed01b46d30
Chris Hamm <chamm@bizstream.com>
2022-01-15 21:07:31 -0500

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="page">
+    <Menu/>
+    <main>
+      <router-view/>
+    </main>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Menu from './components/Menu.vue';
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  components: {
+    Menu,
+  },
+};
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<style lang="scss" scoped>
+  .page {
+    display: flex;
+    height: 100vh;
   }
-}
+
+  main {
+    background-color: rgba(251, 243, 231, 0.5);
+    flex-grow: 1;
+    padding: 60px;
+  }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,62 @@
+<template>
+    <nav>
+      <h1>Cool Drinks</h1>
+      <ul class="nes-list is-disc main-menu nes-container is-rounded">
+        <li v-for="link in links" :key="link.name">
+          <router-link :to="link.path">{{ link.name }}</router-link>
+        </li>
+      </ul>
+      <img src="~@/assets/drank.png" class="drank" alt="">
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data() {
+    return {
+      links: [
+        {
+          name: 'Home',
+          path: '/',
+        },
+        {
+          name: 'About',
+          path: '/about',
+        },
+        {
+          name: 'Team',
+          path: '/team',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  nav {
+    background-image: url('~@/assets/paper.png');
+    padding: 40px;
+    position: relative;
+    width: 260px;
+  }
+  .main-menu {
+    margin-top: 40px !important;
+    padding-left: 42px !important;
+
+    a {
+      color: black;
+      padding-left: 10px;
+    }
+
+    li {
+      margin-bottom: 20px;
+    }
+  }
+  .drank {
+    position: absolute;
+    bottom: -46px;
+    right: -32px;
+  }
+</style>
